d04c448e9e5f53d77f6a37208ba6ea52190c53fb
Harsh Patel <hypharsh@gmail.com>
2019-03-12 14:49:04 -0500

    lklnk;
    knkln

---
+++ Assets/Hacker.cs
@@ -0,0 +1,28 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        showMainMenu();
+
+    }
+
+    void showMainMenu() {
+        Terminal.WriteLine("What would you like to hack into??.");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter your selection: ");
+        Terminal.WriteLine("Great! ");
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
