c1c96b9dc2ef70852d7f95075d3f21b035e582c4
Harsh Patel <hypharsh@gmail.com>
2019-03-15 17:58:27 -0500

    checks passwords now!
    Great

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    enum Screen { Mainmenu , Password , win };
+    int level = 0 ;
+    string password;
+    Screen currentScreen = Screen.Mainmenu;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,16 +19,98 @@ public class Hacker : MonoBehaviour
     }
 
     void showMainMenu() {
+        Terminal.ClearScreen();
         Terminal.WriteLine("What would you like to hack into??.");
         Terminal.WriteLine("Press 1 for the local library");
         Terminal.WriteLine("Press 2 for the police station");
         Terminal.WriteLine("Enter your selection: ");
-        Terminal.WriteLine("Great! ");
-
+        currentScreen = Screen.Mainmenu;
     }
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    void OnUserInput(string input)
+    {
+
+        if (input == "menu")
+        {
+            showMainMenu();
+        }
+        else if (currentScreen.Equals(Screen.Mainmenu))
+        {
+            runMainMenu(input);
+        }
+        else if (currentScreen.Equals(Screen.Password))
+        {
+            runPassword(input);
+        }
+        else {
+            showMainMenu();
+        }
+    
+
+
+
+    }
+
+    void runMainMenu(string input) {
+        currentScreen = Screen.Mainmenu;
+
+        if (input == "1" || input == "2")
+        {
+
+            level = int.Parse(input);
+            startGame();
+
+        }
+        else
+        {
+            Terminal.WriteLine("Write 'menu','1' or '2' to select.");
+        }
+
+    }
+
+    void startGame()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("You have selected LEVEL:" + level);
+        currentScreen = Screen.Password;
+        if (level == 1)
+        {
+            Terminal.WriteLine("Welcome to the local library");
+
+        }
+        else
+        {
+            Terminal.WriteLine("Welcome to the Police Station.");
+
+        }
+
+        Terminal.WriteLine("Enter your Password: ");
+
+
+    }
+
+    void runPassword(string input) {
+        password = input;
+        if (string.Equals(password, "Harsh"))
+        {
+            Terminal.WriteLine("Congratulation You have entered the correct password. ");
+            currentScreen = Screen.win;
+            Terminal.WriteLine("Press any key then enter to play again.");
+
+        }
+        else {
+            Terminal.WriteLine("Please enter the right password. ");
+        
+        }
+
+
+    }
+
+
+
 }
